5fe02c242e57d96eb2488bc50dc2c027c8a5bb18
kass <kisaname@sina.com>
2019-01-25 10:17:51 +0800

    改用邮箱(126邮箱在服务器上出现554 DT:SPM错误；增加循环time.sleep(10)

--- monitor_web.py
+++ monitor_web.py
@@ -3,6 +3,7 @@ from lxml import html
 import json
 import os
 import mailpy
+import time
 
 '''
 class Jianlai(object):
@@ -28,22 +29,20 @@ def init():
     with open(path+'/temp.txt','w') as f:
         json.dump(d1,f)
 
-def test_jianlai():
-    mail_JianLai=mailpy.Email('kisaname@sina.com')
+def test_jianlai(d1):
+    mail_JianLai=mailpy.Email('1358109029@qq.com','keename@sina.com','MEIYOUSINA')
     url='http://book.zongheng.com/showchapter/672340.html'
     response=requests.get(url)
     selector=html.fromstring(response.content)
     chap=selector.xpath('/html/body/div[3]/div[2]/div[2]/div/ul[@class="chapter-list clearfix"]/li')
     lastTitle=selector.xpath('/html/body/div[3]/div[2]/div[2]/div/ul[@class="chapter-list clearfix"]/li/a/text()')
     lastTitle=lastTitle[-1]
-    with open(path+'/temp.txt','r') as f:
-        d1=json.load(f)
     if d1["chapCount"]!=len(chap):
-        d1["chapCOunt"]=len(chap)
+        d1["chapCount"]=len(chap)
         d1["lastTitle"]=lastTitle
         with open(path+'/temp.txt','w') as f:
             json.dump(d1,f)
-    mail_JianLai.send_Email('剑来更新了',lastTitle)
+        mail_JianLai.send_Email('剑来更新了',lastTitle)
 
 if __name__=='__main__':
     path=os.path.abspath('.')
@@ -51,4 +50,9 @@ if __name__=='__main__':
         path='/opt/git_py/server_monitor'
     if not os.path.isfile(path+"/temp.txt"):
         init()
-    test_jianlai()
+    with open(path+'/temp.txt','r') as f:
+        d1=json.load(f)
+    while(True):
+        test_jianlai(d1)
+        time.sleep(10)
+        #print(time.time())
