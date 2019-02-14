aa50fff1ba01de72b5cc9e8d1752cf2f5e2609a0
K_COMPANY_win7 <kisaname@sina.com>
2019-02-14 16:59:32 +0800

    monitor_web.py 添加监测 公众号更新，未调试好

--- monitor_web.py
+++ monitor_web.py
@@ -19,6 +19,8 @@ def Jianlai2dict(obj):
 '''
 path=''
 def init():
+
+    #剑来
     url='http://book.zongheng.com/showchapter/672340.html'
     response=requests.get(url)
     selector=html.fromstring(response.content)
@@ -26,10 +28,16 @@ def init():
     lastTitle=selector.xpath('/html/body/div[3]/div[2]/div[2]/div/ul[@class="chapter-list clearfix"]/li/a/text()')
     lastTitle=lastTitle[-1]
     d1={"name":"剑来","chapCount":len(chap),"lastTitle":lastTitle}
-    with open(path+'/temp.txt','w') as f:
-        json.dump(d1,f)
+    
+    #公众号
 
-def test_jianlai(d1):
+    d2={"name":"公众号刘备教授","lastTitle":lastTitle}
+
+    d0={"jianlai":d1,"weixinso_liubei":d2}
+    with open(path+'/temp.txt','w') as f:
+        json.dump(d0,f)
+
+def test_jianlai(d_tmp,chapCount):
     mail_JianLai=mailpy.Email('1358109029@qq.com','keename@sina.com','MEIYOUSINA')
     url='http://book.zongheng.com/showchapter/672340.html'
     response=requests.get(url)
@@ -37,12 +45,17 @@ def test_jianlai(d1):
     chap=selector.xpath('/html/body/div[3]/div[2]/div[2]/div/ul[@class="chapter-list clearfix"]/li')
     lastTitle=selector.xpath('/html/body/div[3]/div[2]/div[2]/div/ul[@class="chapter-list clearfix"]/li/a/text()')
     lastTitle=lastTitle[-1]
-    if d1["chapCount"]!=len(chap):
-        d1["chapCount"]=len(chap)
-        d1["lastTitle"]=lastTitle
+    if chapCount !=len(chap):
+        d_tmp["jianlai"]["chapCount"]=len(chap)
+        d_tmp["jianlai"]["lastTitle"]=lastTitle
         with open(path+'/temp.txt','w') as f:
-            json.dump(d1,f)
+            json.dump(d_tmp,f)
         mail_JianLai.send_Email('剑来更新了',lastTitle)
+        print('    今天更新了。')
+
+
+def test_weixinso1(d2):
+    pass
 
 if __name__=='__main__':
     path=os.path.abspath('.')
@@ -51,8 +64,18 @@ if __name__=='__main__':
     if not os.path.isfile(path+"/temp.txt"):
         init()
     with open(path+'/temp.txt','r') as f:
-        d1=json.load(f)
+        d_tmp=json.load(f)
+    print('begin loop:')    #提示信息
+    #先取出最新
+    #不断比较
+    #更新后保存
+    #最新 变化
+    
+    #保存参数,避免不断存取，更新时需要更新
+    args1=[d_tmp["jianlai"]["chapCount"],d_tmp["weixinso_liubei"]["lastTitle"]]
     while(True):
-        test_jianlai(d1)
+        test_jianlai(d_tmp,args1[0])   #  剑来
+        test_weixinso1(d_tmp)  #公众号 刘备教授
         time.sleep(10)
         #print(time.time())
+
