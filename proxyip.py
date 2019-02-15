fbe1a7ed7eda3005b18ca85e9de412048470825e
K_COMPANY_win7 <kisaname@sina.com>
2019-02-15 16:26:11 +0800

    add proxyip.py  添加代理功能

---
+++ proxyip.py
@@ -0,0 +1,32 @@
+import requests
+from fake_useragent import UserAgent
+import json
+import random
+
+#pip install fake-useragent
+def get_proxyip_json():
+    # 代理
+    proxyurl='https://proxyscrape.com/api?request=getproxies&proxytype=http&timeout=50&country=GB&ssl=all&anonymity=all'
+    try:
+        ua=UserAgent(verify_ssl=False)    #veriify_ssl  是否验证服务器的SSL证书
+        headers={"User-Agent":ua.random}
+        json_proxy=requests.get(proxyurl,headers=headers)
+        return json_proxy.text.split('\r\n')[:-1]
+    except:
+        return None
+
+#  未：增加排序
+def get_proxyip():
+    json_proxy=get_proxyip_json()
+    return random.choice(json_proxy)
+
+if __name__=="__main__":
+    ua=UserAgent(verify_ssl=False)
+    headers={"User-Agent":ua.chrome}
+    #测试代理  最多10次  ····返回可用代理    ····返回错误
+    json_proxy=get_proxyip_json()
+    proxies_someone=random.choice(json_proxy)
+    print('begin test:'+proxies_someone)
+    proxies={"http":random.choice(proxies_someone)}
+    response=requests.get("http://www.baidu.com",headers=headers,proxies=proxies)
+    print(response.text)
