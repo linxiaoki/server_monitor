a403dec2a0eed0027b171beca29f94beb29df6a8
kass <kisaname@sina.com>
2019-02-26 10:54:39 +0800

    test_jianlai()    #response=requests.get    keep-alive     response=get_url         keep-alive:close 两者有影响吗？

--- monitor_web.py
+++ monitor_web.py
@@ -15,7 +15,6 @@ class Jianlai(object):
     def __init__(self,name,chapCount):
         self.name=name
         self.chapCount=chapCount
-
 def Jianlai2dict(obj):
     return {
         'name': obj.name,
@@ -24,38 +23,39 @@ def Jianlai2dict(obj):
 '''
 
 path=''
-
-def geturl(url,isuseproxy=False):
+# isuseproxy=True  则随机生成代理
+def get_url(url,isuseproxy=False,proxies=None):
     ua=UserAgent(verify_ssl=False,use_cache_server=False)
     headers={"User-Agent":ua.chrome}
-    proxies=None
     if isuseproxy==True:
-        proxies_someone=proxyip.get_proxyip()
-        #proxies={'http':'socks5://'+proxies_someone,'https':'socks5://'+proxies_someone}  # 没试过
-        #proxies={'socket5':proxies_someone}
-        proxies={"http":proxies_someone}
+        proxies=proxyip.get_proxyip()
+    #proxies={'http':'socks5://'+proxies_someone,'https':'socks5://'+proxies_someone}  # 没试过
+    #proxies={'socket5':proxies_someone}
+    proxies={"http":proxies}
+    proxies={"http":proxies,"Connection":'close'}   #  不需要默认的keep-alive
     response=requests.get(url,headers=headers,proxies=proxies)
     return response
 
 def init():
     #剑来
     url='http://book.zongheng.com/showchapter/672340.html'
-    response=geturl(url)
+    response=get_url(url)
     selector=html.fromstring(response.content)
     chap=selector.xpath('/html/body/div[3]/div[2]/div[2]/div/ul[@class="chapter-list clearfix"]/li')
     lastTitle=selector.xpath('/html/body/div[3]/div[2]/div[2]/div/ul[@class="chapter-list clearfix"]/li/a/text()')
     lastTitle=lastTitle[-1]
     d1={"name":"剑来","chapCount":len(chap),"lastTitle":lastTitle}
-    
-    d0={"jianlai":d1}
+
     with open(path+'/temp.json','w') as f:
         json.dump(d0,f)
 
 # 监测：剑来
-def test_jianlai(d1):
+def test_jianlai(d1,proxies):
     mail_user=mailpy.Email('1358109029@qq.com','keename@sina.com','MEIYOUSINA')
     url='http://book.zongheng.com/showchapter/672340.html'
-    response=requests.get(url)
+    proxies={"http":proxies}
+    #response=requests.get(url,proxies=proxies)
+    response=get_url(url,proxies)   # keep-alive   close!
     selector=html.fromstring(response.content)
     chap=selector.xpath('/html/body/div[3]/div[2]/div[2]/div/ul[@class="chapter-list clearfix"]/li')
     lastTitle=selector.xpath('/html/body/div[3]/div[2]/div[2]/div/ul[@class="chapter-list clearfix"]/li/a/text()')
@@ -73,15 +73,26 @@ def test_jianlai(d1):
 #def savenote(url):
     #send_Email(title,url,save@note.youdao.com)
 
-#监测：汇总
-def monitor_web(d_last):
-    d1=test_jianlai(d_last["jianlai"])
-    #d2=test_other(d_last["other"])
-    if d1:
-        d_last={"jianlai":d1}   #d_last={d1,d2....}
-        return d_last
-    return None
- 
+# Loop
+def loop_monitor(nowstamp):
+    proxies=proxyip.get_proxyip()
+    print(proxies)
+    with open(path+'/temp.json','r') as f:
+        d_last=json.load(f)
+    while True:
+        try:
+            if(datetime.now().timestamp()-nowstamp>32400):
+                print(datetime.now().strftime('%a, %b %d %H:%M'))
+                return None
+            d1=test_jianlai(d_last["jianlai"],proxies)
+            if d1:
+                d_tmp={"jianlai":d1}   #d_last={d1,d2....}
+                d_last=d_tmp
+                with open(path+'/temp.json','w') as f:
+                    json.dump(d_last,f)
+            time.sleep(5)
+        except Exception as e1:
+            return str(e1)
 
 if __name__=='__main__':
     path=os.path.abspath('.')
@@ -90,14 +101,14 @@ if __name__=='__main__':
     if not os.path.isfile(path+"/temp.json"):
         init()
 
-    with open(path+'/temp.json','r') as f:
-        d_last=json.load(f)
     print('begin loop:')
+    nowstamp=datetime.now().timestamp()
+    mail_error=mailpy.Email('1358109029@qq.com','keename@sina.com','MEIYOUSINA')
     while True:
-        d_tmp=monitor_web(d_last)
-        if d_tmp:       #实时更新
-            d_last=d_tmp
-            with open(path+'/temp.json','w') as f:
-                json.dump(d_last,f)
+        errormsg=loop_monitor(nowstamp)
+        if not(errormsg):
+            break
+        print(errormsg)
+        url='http://book.zongheng.com/showchapter/672340.html'
         time.sleep(5)
- 
+        mail_error.send_Email("monitor_web 运行出错",errormsg)
