3c40ea23a867885f1373b430583d5aa9d5a06b0c
K_COMPANY_win7 <kisaname@sina.com>
2019-08-09 16:00:58 +0800

    add -u (dev)

--- monitor_web.py
+++ monitor_web.py
@@ -8,7 +8,12 @@ from fake_useragent import UserAgent
 import mailpy
 import proxyip
 from datetime import datetime
-import random
+import random,hashlib,string
+
+#原url:  http://book.zongheng.com/showchapter/672340.html
+#现url:  url="https://m.zongheng.com/h5/ajax/chapter/list"
+#        data={'h5':1,'bookId':672340,'pageNum':1,'pageSize':20,'chapterId':0,'asc':1}
+
 
 '''  
 class Jianlai(object):
@@ -25,121 +30,103 @@ def Jianlai2dict(obj):
 
 path=''
 
+#把每一次print 都写入文件 
 def output(str1):
-    with open(path+'/run_log_test_11','ab') as f:
+    #txt+=(str1+'\n')
+    #添加时间，在某个时间点输出？
+    with open(path+'/run_log_test','ab') as f:
         if(not isinstance(str1,str)):
             str1=str(str1)
         str1=str1+'\n'
         f.write(str1.encode('utf-8'))
 #print=output
 
-# isuseproxy=True  则随机生成代理  或者 直接接受代理参数 proxies
-def get_url(url,isuseproxy=False,proxies=None):
+
+# 获取  请求json需要的参数：随机的Herders,Cookies,代理proxies
+def getRandomHeader(isUseProxy=True):
+    #随机获取字符串计算MD5值作为ZHID,返回 cookies
+    def getRamdomCookies():
+        zhid=''.join([random.choice(string.ascii_letters+string.digits) for i in range(20)])
+        md5=hashlib.md5()
+        md5.update((zhid).encode('utf-8'))
+        zhid=md5.hexdigest().upper()
+        return {'ZHID':zhid}
+    
     ua=UserAgent(verify_ssl=False,use_cache_server=False)
-    headers={"User-Agent":ua.chrome}
-    if isuseproxy==True:
+    headers={"User-Agent":ua.chrome,"Referer":"https://m.zongheng.com/h5/chapter/list?bookid=672340"}
+    cookies=getRamdomCookies()
+    proxies=None
+    if isUseProxy==True:
         proxies=proxyip.get_proxyip()
+        proxies={"http":proxies,"Connection":"close"}  # keep-alive:false  测试
+    return {'headers':headers,'cookies':cookies,'proxies':proxies}
+
+
+# isuseproxy=True  则随机生成代理  或者 直接接受代理参数 proxies
+def get_url(url,isUseProxy=False,proxies=None,cookies=None,headers=None):
+    if isUseProxy==True:
+        proxies=proxyip.get_proxyip()
+        print('init->get_url->proxy:'+proxies)
+        proxies={"http":proxies,"Connection":"close"}  # keep-alive:false  测试 
     #proxies={'http':'socks5://'+proxies_someone,'https':'socks5://'+proxies_someone}  # 没试过
     #proxies={'socket5':proxies_someone}
-    proxies={"http":proxies,"Connection":"close"}  # keep-alive:false  测试 
-    response=requests.get(url,headers=headers,proxies=proxies)
+    response=requests.get(url,headers=headers,proxies=proxies,cookies=cookies)
     return response
 
 
 def init():
     #剑来
-    url='http://book.zongheng.com/showchapter/672340.html'
-    response=get_url(url)
-    selector=html.fromstring(response.content)
-    chap=selector.xpath('/html/body/div[3]/div[2]/div[2]/div/ul[@class="chapter-list clearfix"]/li')
-    lastTitle=selector.xpath('/html/body/div[3]/div[2]/div[2]/div/ul[@class="chapter-list clearfix"]/li/a/text()')
-    lastTitle=lastTitle[-1]
-    d1={"name":"剑来","chapCount":len(chap),"lastTitle":lastTitle}
+    url="https://m.zongheng.com/h5/ajax/chapter/list?h5=1&bookId=672340&pageNum=1&chapterId=0&pageSize=20&asc=1"
+    #url='http://book.zongheng.com/showchapter/672340.html'
+    #response=get_url(url,isuseproxy=True)  #测试 但是出错不能循环
+    kwargs=getRandomHeader(isUseProxy=False)
+    response=get_url(url,**kwargs)  #isUseProxy=True
+    chaplist=json.loads(response.content.decode('utf-8'))
+    chapterName=chaplist['chapterlist']['chapters'][0]['chapterName']
+    chapterCount=chaplist['chapterlist']['chapterCount']
+    d1={"name":"剑来","chapCount":chapterCount,"lastTitle":chapterName}
     d0={"jianlai":d1}
     with open(path+'/temp.json','w') as f:
         json.dump(d0,f)
 
+
 # 监测：剑来
-def test_jianlai(d1,proxies):
+def test_jianlai(d1,request_kwargs):
     mail_user=mailpy.Email(['1358109029@qq.com'],'keename@sina.com','MEIYOUSINA')
     #mail_user=mailpy.Email(['1358109029@qq.com','1225517060@qq.com'],'keename@sina.com','MEIYOUSINA')
-    url='http://book.zongheng.com/showchapter/672340.html'
-    #proxies={"http":proxies}
-    #response=requests.get(url,proxies=proxies)
-    response=get_url(url,proxies=proxies)   # keep-alive   close!
-    selector=html.fromstring(response.content)
-    chap=selector.xpath('/html/body/div[3]/div[2]/div[2]/div/ul[@class="chapter-list clearfix"]/li')
-    lastTitle=selector.xpath('/html/body/div[3]/div[2]/div[2]/div/ul[@class="chapter-list clearfix"]/li/a/text()')
-    lastTitle=lastTitle[-1]
-    chapCount=d1["chapCount"]
-    print(chapCount)
-    if chapCount < len(chap):
-        d1["chapCount"]=len(chap)
-        d1["lastTitle"]=lastTitle
-        mail_user.send_Email('剑来更新了',lastTitle)
-        print('    今天剑来更新:'+lastTitle+'('+datetime.now().strftime("%a, %b %d %H:%M")+')')
+
+    url="https://m.zongheng.com/h5/ajax/chapter/list?h5=1&bookId=672340&pageNum=1&chapterId=0&pageSize=20&asc=1"
+    response=get_url(url,**request_kwargs)   # keep-alive   close!
+    chaplist=json.loads(response.content.decode('utf-8'))
+    chapterName=chaplist['chapterlist']['chapters'][0]['chapterName']
+    chapterCount=chaplist['chapterlist']['chapterCount']
+
+    if chapterCount > d1["chapCount"] :
+        d1["chapCount"]=chapterCount
+        d1["lastTitle"]=chapterName
+        mail_user.send_Email('剑来更新了',chapterName)
+        print('    今天剑来更新:'+chapterName+'('+datetime.now().strftime("%a, %b %d %H:%M")+')')
         return d1
     return None
 
-def get_jlChapcount(proxies):
-    url='http://book.zongheng.com/showchapter/672340.html'
-    response=get_url(url,proxies=proxies)   # keep-alive   close!
-    selector=html.fromstring(response.content)
-    #chap=selector.xpath('/html/body/div[3]/div[2]/div[2]/div/ul[@class="chapter-list clearfix"]/li')
-    lastTitle=selector.xpath('/html/body/div[3]/div[2]/div[2]/div/ul[@class="chapter-list clearfix"]/li/a/text()')
-    lastTitle=lastTitle[-1]+"  "+str(len(lastTitle))
-    return lastTitle
 
 # 保存笔记到有道云
 #def savenote(url):
     #send_Email(title,url,save@note.youdao.com)
 
-# Loop
-def loop_monitor(nowstamp):
-    try:
-        proxies=proxyip.get_proxyip()
-        print('    '+proxies)
-        with open(path+'/temp.json','r') as f:
-            d_last=json.load(f)
-    except Exception as e1:
-        print('    loop_monitor->loop->catch error0')
-        return str(e1)
-    while True:
-        try:
-            #间隔一段时间输出日志
-            if(datetime.now().timestamp()-nowstamp>28000):
-                print(datetime.now().strftime('%a, %b %d %H:%M'))
-                return None
-            deltime=(datetime.now().timestamp()-nowstamp)%3600
-            if(deltime<18):
-                print("          我在运行中哦")
-                print("          "+get_jlChapcount(proxies))
-                print("          "+d_last["jianlai"]["lastTitle"]+"  "+str(d_last["jianlai"]["chapCount"]))
-                print("          "+datetime.now().strftime('%a, %b %d %H:%M'))
-            d1=test_jianlai(d_last["jianlai"],proxies)
-            if d1:
-                d_tmp={"jianlai":d1}   #d_last={d1,d2....}
-                d_last=d_tmp
-                with open(path+'/temp.json','w') as f:
-                    json.dump(d_last,f)
-            time.sleep(random.randint(9,15))
-        except Exception as e1:
-            print('    loop_monitor->loop->catch error')
-            return str(e1)
 
-def loop_monitor_():
+def loop_monitor():
     try:
-        proxies=proxyip.get_proxyip()
-        print('    '+proxies)
+        request_kwargs=getRandomHeader()
         with open(path+'/temp.json','r') as f:
             d_last=json.load(f)
     except Exception as e1:
-        print('    loop_monitor->loop->catch error-> proxy')
+        print('    loop_monitor->loop->catch error-> getRandomHeader')
         return str(e1)
     while True:
         try:
             print('test jianlai')
-            d1=test_jianlai(d_last["jianlai"],proxies)
+            d1=test_jianlai(d_last["jianlai"],request_kwargs)
             if d1:
                 d_tmp={"jianlai":d1}   #d_last={d1,d2....}
                 d_last=d_tmp
@@ -152,8 +139,12 @@ def loop_monitor_():
 
 if __name__=='__main__':
     path=os.path.abspath('.')
-    if path=='/root':   #自动运行脚本时 centos7相对路径为/root
+    if path=='/root':   #自动运行脚本时 centos7相对路径为/root ,需要手动改路径
         path='/opt/git_py/server_monitor'
+    
+    path=path+'/ini' #配置文件
+    if not os.path.exists(path):
+        os.mkdir(path)
     if not os.path.isfile(path+"/temp.json"):
         init()
 
@@ -164,8 +155,8 @@ if __name__=='__main__':
     print(datetime.now())
     while True:
         #errormsg=loop_monitor_(nowstamp)
-        errormsg=loop_monitor_()
-        print('loop_monitor done')
+        errormsg=loop_monitor()
+        print('循环监测时发现错误，重新开始循环')
         if not(errormsg):
             print('    错误次数：'+str(errortimes))
             print('运行结束'.center(20,'-'))
--- proxyip.py
+++ proxyip.py
@@ -3,12 +3,18 @@ import requests
 from fake_useragent import UserAgent
 import json
 import random
+import socks
+from datetime import datetime
+import os
+
+path=''  #注意在 linux 系统中，运行脚本时的 . 目录时 /root
 
 #pip install fake-useragent
-def get_proxyip_json(proxytype='http'):
+def get_proxyip_file(proxytype='http'):
     # 代理
     if proxytype=='http':
-        proxyurl='https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=50&country=GB&ssl=all&anonymity=all'
+        # &country=GB
+        proxyurl='https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=100&country=all&ssl=all&anonymity=all'
     elif proxytype=='socket4':
                 #https://api.proxyscrape.com/?request=getproxies&proxytype=socks4&timeout=250&country=all
         proxyurl='https://api.proxyscrape.com/?request=getproxies&proxytype=socks4&timeout=100&country=all'
@@ -18,17 +24,50 @@ def get_proxyip_json(proxytype='http'):
     try:
         ua=UserAgent()    # verify_ssl=False   veriify_ssl  是否验证服务器的SSL证书
         headers={"User-Agent":ua.random}
-        json_proxy=requests.get(proxyurl,headers=headers)
-        return json_proxy.text.split('\r\n')[:-1]
-    except:
+        #print(socks.get_default_proxy())    #mail.py  使用全局代理 发送邮件 测试
+        response=requests.get(proxyurl,headers=headers)
+        return response.text
+    except Exception:
         return None
+
 
 #  未：增加排序
 def get_proxyip(proxytype='http'):
-    json_proxy=get_proxyip_json(proxytype)
-    return random.choice(json_proxy)
+    path=os.path.abspath('.')
+    if path=='/root':   #自动运行脚本时 centos7相对路径为/root ,需要手动改路径
+        path='/opt/git_py/server_monitor'
+    path=path+'/ini'
+    if(not os.path.exists(path)):
+        os.mkdir(path)
+    proxies_filename=path+"/proxy_list.txt"
+
+    modified_timestamp=0
+    if os.path.isfile(proxies_filename):  #（没文件 或者 时间长 ） 重新写入     （有文件 并且 时间 短）  直接读取
+        modified_timestamp=os.path.getmtime(proxies_filename)
+    now_timestamp=datetime.now().timestamp()
+
+    if (modified_timestamp != 0) and (now_timestamp-modified_timestamp)<3600:
+        with open('ini/proxy_list.txt','r') as f:
+            proxy_list=f.read()
+            proxy_list=proxy_list.split('\n\n')[:-1]
+    else:
+        proxy_list=get_proxyip_file(proxytype)
+        if proxy_list is None:
+            raise ValueError('invalid proxy value: None.(proxyip.py)')
+        with open('ini/proxy_list.txt','w') as f:
+            f.write(proxy_list)
+        proxy_list=proxy_list.split('\r\n')[:-1] 
+    return random.choice(proxy_list)
+
+#读取文件 ini/proxy_list.txt, 访问一个网址，测试响应时间并排序  还是筛选？
+def sort_proxy():
+    #def choice_proxy():
+    pass
 
 if __name__=="__main__":
+    get_proxyip()
+
+    '''
     #for test proxyip: get http/socket4/socket5
     ua=UserAgent(verify_ssl=False)
     headers={"User-Agent":ua.chrome}
@@ -40,4 +79,6 @@ if __name__=="__main__":
     proxies={"socket5":proxies_someone}
     response=requests.get("http://www.baidu.com",headers=headers,proxies=proxies)
     print('complete')
+    '''
+
     
